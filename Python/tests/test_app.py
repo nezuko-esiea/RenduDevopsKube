73e632f5461121b35524546a1715a2322bb50ba3
Bdj Dyho <dyhia.boudjema@et.esiea.fr>
2026-01-16 19:07:50 +0100

    dyhiaboudjema-dontdeletemyfile---

--- Python/tests/test_app.py
+++ Python/tests/test_app.py
@@ -1,5 +1,10 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
-from api import create_app # Adapte selon le nom de ta fonction dans api.py
+from api import create_app 
 
 @pytest.fixture
 def client():
